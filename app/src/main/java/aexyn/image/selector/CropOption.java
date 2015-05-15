a7d9aff4c976e36518a6a0542c7624efcfa69bcf
ahuja <ankitahuja007@gmail.com>
2015-05-15 21:41:25 +0530

    android image selection and crop

---
+++ app/src/main/java/aexyn/image/selector/CropOption.java
@@ -0,0 +1,10 @@
+package aexyn.image.selector;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+public class CropOption {
+	public CharSequence title;
+	public Drawable icon;
+	public Intent appIntent;
+}
